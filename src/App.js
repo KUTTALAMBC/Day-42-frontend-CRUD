ce840cfa203a0b64b8732ab08199cadee594f1e8
kuttalam <kuttalambc@gmail.com>
2022-06-20 19:34:33 +0530

    fist step

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import UserList from './userList';
+import UserCreate from './userCreate';
+import EditUser from './editUser';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='container'>
+        <Routes>
+          <Route path="/" element={<UserList />} />
+          <Route path="/create" element={<UserCreate />} />
+          <Route path="/edit-user/:id" element={<EditUser />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
